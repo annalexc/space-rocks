f09418c4af7b15d870b280cf3f00f6a6829067af
Anna Concepcion <anna.concepcion@gmail.com>
2016-06-03 22:32:58 -0400

    Updates styles to ensure that scaled earth and moon view are responsive

--- client/assets/js/scripts.js
+++ client/assets/js/scripts.js
@@ -1,13 +1,19 @@
 console.log("Space asteroids!");
 
 $(function(){
-  // **** LIGHT UP THE NIGHT SKY ****//
+  //********************************//
+  //**** LIGHT UP THE NIGHT SKY ****//
+  //********************************//
   var starField = $("#space-bg");
   createStarryNight(starField);
   toggleScaleViewHandler();
   
   //**** ADD ASTEROIDS ****//
-  renderAsteroids();
+  // renderAsteroids();
+  
+
+
+
   renderGlobe();
 
   // window.onresize = resizeCanvas;
@@ -23,21 +29,17 @@ function toggleScaleViewHandler(){
   $("#scale").on("click", function(){
     var wrapperScaled = $("#wrapper-scaled");
     var wrapper = $("#wrapper");
-
+    
     if($(wrapperScaled).hasClass('hidden')){
-      $(wrapper).fadeOut(500, function(){
-        $(wrapper).addClass('hidden');
-        $(wrapperScaled).fadeIn(1000, function(){
-          $(wrapperScaled).removeClass('hidden');
-        });
-      });
+      $(wrapper).fadeOut(750, function(){
+        $(wrapperScaled).removeClass('hidden').fadeIn(1200);
+        $(this).addClass('hidden');
+      });  
     } else {
-      $(wrapperScaled).fadeOut(500, function(){
-        $(wrapperScaled).addClass('hidden');
-        $(wrapper).fadeIn(1000, function(){
-          $(wrapper).removeClass('hidden');
-        });
-      });
+      $(wrapperScaled).fadeOut(750, function(){
+        $(wrapper).removeClass('hidden').fadeIn(1200);
+        $(this).addClass('hidden');
+      });    
     };
   });
 };
