a061cebff51a3b9c42ce37dd1c05dd169d4ad24c
Anna Concepcion <anna.concepcion@gmail.com>
2016-09-18 15:06:51 -0400

    Center year display in styles

--- app.js
+++ app.js
@@ -5,11 +5,9 @@ var express         = require('express'),
     app             = express(),  
     apiAsteroidRouter = require('./server/routes/api/asteroids.js');
 
-
 mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/spacerocks" );
 
 app.set('views', path.join(__dirname, 'client/views'));
-
 
 app.use(morgan('dev'));
 
@@ -29,8 +27,6 @@ app.get("/data/:file", function(req, res){
   console.log("Sending " + req.params.file );
   res.sendFile( path.resolve( __dirname + "/data/" + req.params.file ) );
 });
-
-
 
 
 // process.env.PORT is needed for when we deploy to Heroku
